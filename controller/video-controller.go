61dd6c2942de40afe370817998afb4d86f4f13c1
daisuke8000 <daisuke12108000@gmail.com>
2021-10-02 21:42:57 +0900

    templates

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -6,11 +6,13 @@ import (
 	"github.com/daisuke8000/gin_dev/validators"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"net/http"
 )
 
 type VideoController interface {
 	FindAll() []entity.Video
 	Save(ctx *gin.Context) error
+	ShowAll(ctx *gin.Context)
 }
 
 type controller struct {
@@ -43,5 +45,14 @@ func (c *controller) Save(ctx *gin.Context) error{
 	}
 	c.service.Save(video)
 	return nil
+}
+
+func (c *controller) ShowAll(ctx *gin.Context) {
+	videos := c.service.FindAll()
+	data := gin.H{
+		"title": "Video Page",
+		"videos": videos,
+	}
+	ctx.HTML(http.StatusOK, "index.html", data)
 }
 
